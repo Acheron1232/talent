ea61d4648181ea832f59b3e58aafbe312614b076
Acheron1232 <aryemfedorov@gmail.com>
2025-10-01 18:42:35 +0300

    merge

--- auth-server/src/main/java/org/acheron/authserver/config/Oauth2AccessTokenCustomizer.java
+++ auth-server/src/main/java/org/acheron/authserver/config/Oauth2AccessTokenCustomizer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.acheron.authserver.service.UserService;
 import org.acheron.user.UserDto;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
@@ -18,6 +19,9 @@ public class Oauth2AccessTokenCustomizer implements OAuth2TokenCustomizer<JwtEnc
 
     @Override
     public void customize(JwtEncodingContext context) {
+        if(!context.getRegisteredClient().getAuthorizationGrantTypes().equals(AuthorizationGrantType.AUTHORIZATION_CODE)){
+            return;
+        }
         if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
             context.getClaims().claims(claims -> {
                 Object principal = context.getPrincipal().getPrincipal();
